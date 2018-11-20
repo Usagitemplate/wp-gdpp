b92a7da9d6ca7e735ca73b13419ba8957654f2dd
ꦧꦭ꧀ <iqbalrifai@waifu.club>
2018-11-21 02:52:43 +0700

    🤔🤔

---
+++ gdpp.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: wp-gdpp
+Plugin URI:  https://github.com/Usagitemplate/wp-gdpp/
+Description: Wordpress Plugins Google Drive Player Proxy
+Version:     0.1
+Author:      Usagitemplate
+Author URI:  https://usagitemplate.com
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: wp-gdpp
+Domain Path: /languages
+*/
